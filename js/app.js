93a7f75568e6aabce7b75222aae26919964e8dcd
Qais Alshorman <qaishorman20@gmail.com>
2021-06-21 16:22:43 +0300

    modified

---
+++ js/app.js
@@ -0,0 +1,44 @@
+'use strict'
+alert ('the answer is yes or no' )
+  let Qus1=  prompt("I am from a north city called Irbid do you hear about ?");
+  if ( Qus1.toUpperCase =='Yes'){
+console.log(Qus1);
+alert("you enter" +  answer + "correct")}
+
+else {
+alert ('wish to know more about it ');
+}
+
+let Qus2=  prompt("am i in second  dedicated ") ;
+Qus2.toUpperCase(Qus2);
+
+if (Qus2 == "NO"){
+    alert('you answered'+ Qus2 +'correct')
+    
+}
+
+else {
+
+    alert ('try agine in other time')
+}
+
+let Qus3=  prompt("maths and phisic are core of my major am I a teacher ") ;
+if (Qus3.toUpperCase() =='No'){
+
+console.log(Qus3);
+alert("you enter"+ Qus3 +'correct');}
+else {
+    alert ('you did not git it ')
+}
+
+let Qus4=  prompt("studing and learning is good hoppies ,tell us about you  ") ;
+Qus4.toUpperCase();
+console.log(Qus4);
+alert("you enter"+ Qus4 );
+
+let Qus5=  prompt("Are happy to visit my page ") ;
+if (Qus5.toUpperCase() == 'YES'){
+    alert (' how lovely you are , thank you ') ;}
+else {
+console.log(Qus5);
+alert("you enter"+  Qus5); }
